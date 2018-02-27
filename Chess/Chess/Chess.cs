a1cbab5f05e7afcea4fc27afd3a96e71bd955b9d
Oleg <Oleg.Zarevych@gmail.com>
2018-02-27 20:52:36 +0200

    Create Chess project and add basic templates

---
+++ Chess/Chess/Chess.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess
+{
+    public class Chess
+    {
+        private string fen;
+
+        public Chess(string fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+        {
+            this.fen = fen;
+        }
+
+        public Chess Move(string move)
+        {
+            return new Chess(fen);
+        }
+
+        public char GetFigureAt(int x, int y)
+        {
+            return '.';
+        }
+    }
+}
